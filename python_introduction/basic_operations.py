2efd51b6d27932937b0cf423bdfc0415f12d318a
Amatotsero Divine Onome <divineamats@yahoo.com>
2024-12-16 05:06:15 +0100

    Added: New directory and basic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#To practice baxic arithmetic operations in python by performing predefined calculations
+
+#declare two variables each to hold an integer number
+number1 = 10
+number2 = 5
+
+#Declare variables to store the sum, diff and product of the numbers
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+
+#output the answers to user
+print ("Addition of", number1, "and", number2, "is", sum)
+print ("Substraction of", number2, "and", number1, "is", diff)
+print ("Multiplication of", number1, "and", number2, "is", product)
