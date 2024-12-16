16d9644133cec27a49caae028763f70390435f52
Amatotsero Divine Onome <divineamats@yahoo.com>
2024-12-16 11:12:54 +0100

    Added a script to calculate interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#applying basic python arithmetic operations and variable assignments to calculate simple interest
+
+#declare the variables for principal, time and interest rate.
+
+principal = 1000
+rate = float(0.05)
+time = 3
+
+#calculate simple interest using formula I=PRT
+interest = principal * rate * time
+
+print ('The simple interest is:', interest)
