4d543f89489d7c594c0a0653a2afb497df16a238
Amatotsero Divine Onome <divineamats@yahoo.com>
2024-12-24 12:37:13 +0100

    This should be the final correction

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,10 +3,9 @@
 #Prompt user for number which the multiplication table should be generated
 
 number = int(input("Enter a number to see its multiplication table: "))
-X = number
+
 #generate multiplication table using for loops
-for Y in range(1, 11):
-    product = float(X * Y)
-    Z = product
-    print(X,"*",Y,"=", Z)
+for num in range(1, 11):
+    product = number *  num
+    print(f"{number} * {num} = {product}")
     
