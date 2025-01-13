2ce53f012060e0a0c879812eee893346f411a1c0
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-01-13 14:58:28 +0100

    Made corrections to the arithmetic function script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,10 +8,10 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             print('Invalid')
+        else:
+            return num1 / num2
        
 perform_operation(num1=None, num2=None, operation=None)           
             
