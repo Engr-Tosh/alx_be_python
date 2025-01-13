beecbb3a888a83b2c57cf904fec5e029fcc5e7d2
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-01-13 14:43:50 +0100

    Made script code corrections

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 #Function to perform basic arithmetic operations
 
-def perform_operation(num1, num2, operation = ('add', 'subtract', 'multiply', 'divide') ):
+def perform_operation(num1, num2, operation):
     match operation:
         case 'add':
             return num1 + num2
