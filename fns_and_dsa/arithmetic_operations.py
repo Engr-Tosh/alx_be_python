92746f5d5f467185651bf037b4373ee72eb93e9e
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-01-13 14:24:29 +0100

    Added: arithmetic operation function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#Function to perform basic arithmetic operations
+
+def perform_operation(num1 = None, num2 = None, operation = ('add', 'subtract', 'multiply', 'divide')):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print('Invalid')
+        
+perform_operation()           
+            
