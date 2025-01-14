e2ceeb5d870de06e7ff2393d58e936c0730f18a0
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-01-14 12:59:34 +0100

    Added: A script exploring the datetime module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta 
+
+now_date_time = datetime.now()
+
+def display_current_datetime():
+    current_date = now_date_time.date()
+    print(current_date)
+
+print(f"Current date and time: {now_date_time}")
+
+def calculate_future_date():
+    future_date = now_date_time + timedelta(days=int(input("Enter the number of days to add to the current date: ")))
+    print(f"Future date: {future_date}")
+
+calculate_future_date()
