d21ce3efdd0023b69da8e23004a86a2cdd993c1b
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-01-22 05:02:28 +0100

    Added: A basic class and a script to utilize thecreated class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,29 @@
+#Creating a simple bank account class
+
+#Define the bank acoount class
+class BankAccount():
+    #Use __init__ method to initialize acc blc attribute
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        self.initial_balance = 0     #Default value of bank acc blc
+        
+    
+    #Now to practice encapsulation behaviour
+    def deposit(self, amount):
+        #This method should add a specific amount to account balance
+        return self.account_balance + amount
+
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            return True
+        else:
+            return False
+    
+    def current_balance(self):
+        new_balance = self.initial_balance + self.account_balance
+        return new_balance
+        
+
+    def display_balance(self):
+        print(f"Current Balance: {self.current_balance()}")
+        
