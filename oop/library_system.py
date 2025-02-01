2d46eb61ce84bcbfcdf462bd4a45e68a8207b949
Amatotsero Divine Onome <divineamats@yahoo.com>
2025-02-01 07:17:53 +0100

    I know not what I'm doing

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,18 +6,28 @@ class Book:
         self.title = str(title)
         self.author = str(author)
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+        
+
 #Creating the inherited/derived classes
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_size}"
         
         
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
 
 #Creating class to demonstrate composition
 class Library:
